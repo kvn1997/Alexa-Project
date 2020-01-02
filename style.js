f6cbfd5018e2f3052d239448ee817c0ed1c950f5
Jony Vega <jhonvegmun@hotmail.com>
2020-01-02 18:14:31 +0100

    saqué el boton del section para hacer el efecto(falta aun) con js()

--- style.js
+++ style.js
@@ -1,53 +1,32 @@
 $(document).ready(function() {
 
-
-
-
     //llamo a la funcion que movera la foto cuando el scroll este en la posicion
     $(document).on("scroll", function() {
         var imgAlePerf = $('#alexa-perfil');
+        var botonProy = $("btn-proyectos");
         var desplazamientoActual = $(document).scrollTop();
 
-
-
+        //estos 4(250, 40, 300 y 400) valores son importantes para lograr el efecto de la imagen que sube
+        //mientras que vas bajando en la pagina web. Y también la linea imgAlePerf.stop().animate();
         if (desplazamientoActual >= 250) {
             imgAlePerf.animate({ marginTop: 40 }, 1000);
         }
         if (desplazamientoActual < 300) {
             imgAlePerf.stop().animate();
+            imgAlePerf.stop().animate();
             imgAlePerf.animate({ marginTop: 400 });
         }
         console.log(desplazamientoActual);
+
+        //condiciones para el boton
+        if (desplazamientoActual >= 1100) {
+            botonProy.animate({ marginTop: 35 }, 1000);
+        }
+        // if (desplazamientoActual < 1000) {
+        //     botonProy.stop().animate();
+        //     botonProy.stop().animate();
+        //     botonProy.animate({ marginTop: -35 });
+        // }
+
     });
-
-    // $(window).scroll(function() {
-    //     var imgAlePerf = $('#alexa-perfil');
-    //     var top = $(window).scrollTop();
-    //     if (top < 300) {
-    //         imgAlePerf.fadeIn();
-    //     }
-    // });
-
-
-
-
-
-    // $(document).on("scroll", function() {
-    //     var imgAlePerfil = $('#alexa-perfil');
-    //     var valorDesplazamiento = $(document).scrollTop();
-
-    //     function bajarImgAlexaPerf() {
-    //         imgAlePerfil.animate({ marginBottom: 15 }, 1000)
-
-    //     }
-
-    //     if (valorDesplazamiento < 300) {
-    //         console.log("sdgsdg");
-    //         bajarImgAlexaPerf();
-    //     }
-    //     console.log(desplazamientoActual); //aqui me muestra en tiempo real la posicion de mi scroll
-    // });
-
-
-
 });

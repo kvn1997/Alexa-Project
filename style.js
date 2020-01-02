ce9444d363f8e658c6650b1d5a078e4666fb9586
Jony Vega <jhonvegmun@hotmail.com>
2020-01-02 17:30:17 +0100

    imagen deslizable hasta este punto

--- style.js
+++ style.js
@@ -1,21 +1,52 @@
-'use strict'
-
-
 $(document).ready(function() {
-    //alert("funcio");
+
+
+
 
     //llamo a la funcion que movera la foto cuando el scroll este en la posicion
     $(document).on("scroll", function() {
+        var imgAlePerf = $('#alexa-perfil');
         var desplazamientoActual = $(document).scrollTop();
-        if (desplazamientoActual < 310) {
-            $("#alexa-perfil").fadeOut('slow');
-            $("alexa-perfil").css("margin-top", "20px");
-        } else {
-            $("#alexa-perfil").fadeIn();
+
+
+
+        if (desplazamientoActual >= 250) {
+            imgAlePerf.animate({ marginTop: 40 }, 1000);
         }
-        console.log(desplazamientoActual); //aqui me muestra en tiempo real la posicion de mi scroll
+        if (desplazamientoActual < 300) {
+            imgAlePerf.stop().animate();
+            imgAlePerf.animate({ marginTop: 400 });
+        }
+        console.log(desplazamientoActual);
     });
 
+    // $(window).scroll(function() {
+    //     var imgAlePerf = $('#alexa-perfil');
+    //     var top = $(window).scrollTop();
+    //     if (top < 300) {
+    //         imgAlePerf.fadeIn();
+    //     }
+    // });
+
+
+
+
+
+    // $(document).on("scroll", function() {
+    //     var imgAlePerfil = $('#alexa-perfil');
+    //     var valorDesplazamiento = $(document).scrollTop();
+
+    //     function bajarImgAlexaPerf() {
+    //         imgAlePerfil.animate({ marginBottom: 15 }, 1000)
+
+    //     }
+
+    //     if (valorDesplazamiento < 300) {
+    //         console.log("sdgsdg");
+    //         bajarImgAlexaPerf();
+    //     }
+    //     console.log(desplazamientoActual); //aqui me muestra en tiempo real la posicion de mi scroll
+    // });
 
 
 
